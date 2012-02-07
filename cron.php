eeb9ba21c00d864c9b57b229d8012edb09bd01c5
Anthony Ferrara <ircmaxell@gmail.com>
2012-02-07 08:14:29 -0500

    Adding scheduling support for individual sources

--- cron.php
+++ cron.php
@@ -11,10 +11,17 @@ $mysqli = new ircmaxell\com\Sources\MySQLi($config['database']);
 $mapper = new ircmaxell\com\DataMappers\Post($mysqli);
 
 foreach ($config['sources'] as $name => $config) {
-    $config += array('constructorArgs' => array());
+    $config += array('constructorArgs' => array(), 'frequency' => 0);
+    $sql = 'SELECT `value` FROM cron_info WHERE `name` = ?';
+    $result = $mysqli->query($sql, array($name . '_last_run'))->fetch_assoc();
+    if ($result && $result['value'] + $config['frequency'] > time()) {
+        continue;
+    }
     $r = new ReflectionClass('\\ircmaxell\\com\\Models\\Source\\' . $name);
     $source = $r->newInstanceArgs($config['constructorArgs']);
     $data = $source->getLatestPosts(0, 10);
     echo "Saving " . count($data) . " Items From $name\n";
     $mapper->saveAll($data);
+    $sql = 'INSERT INTO `cron_info` (`name`, `value`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `value` = VALUES(`value`);';
+    $mysqli->query($sql, array($name . '_last_run', time()));
 }
